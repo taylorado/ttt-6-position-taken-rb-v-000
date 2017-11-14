6fdddd5e9048edfdeb8feaf31f4f31a8fd554267
Taylor Donlan <taylordonlan@gmail.com>
2017-11-14 20:05:21 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,6 @@
 def position_taken?(board, index)
   if (board[index] == "X" || board[index] == "O")
     true
-  elseif (board[index] == " " || board[index] == "")
+  elsif (board[index] == " " || board[index] == "")
    false
 end
